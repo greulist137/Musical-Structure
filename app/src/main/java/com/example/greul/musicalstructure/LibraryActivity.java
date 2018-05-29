bdf9616c4b65be353d3431b61802985101be0583
Steven Greulich <greulich.steven@gmail.com>
2018-05-28 17:44:32 -0700

    Commit - Creation of Template of Library
    The purpose of this commit is for the creation of a base template of the Library Activity

---
+++ app/src/main/java/com/example/greul/musicalstructure/LibraryActivity.java
@@ -0,0 +1,16 @@
+package com.example.greul.musicalstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class LibraryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library_view);
+
+    }
+}
--- app/src/main/java/com/example/greul/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/greul/musicalstructure/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.greul.musicalstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout library = (LinearLayout) findViewById(R.id.library);
+        library.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LibraryIntent = new Intent(MainActivity.this, LibraryActivity.class);
+                startActivity(LibraryIntent);
+            }
+        });
+
     }
 }
