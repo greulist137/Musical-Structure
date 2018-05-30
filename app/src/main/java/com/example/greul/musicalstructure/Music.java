d56e9c32487aa066871cfb79c1c6f6823e2db776
Steven Greulich <greulich.steven@gmail.com>
2018-05-29 19:31:03 -0700

    Commit - Creation of Music Class
    The purpose of this commit is for the creation of the "Music" class.

---
+++ app/src/main/java/com/example/greul/musicalstructure/Music.java
@@ -0,0 +1,17 @@
+package com.example.greul.musicalstructure;
+
+public class Music {
+
+    private String mAlbum;
+    private String mArtist;
+    private String[] mSongs;
+    private double[] mprices;
+
+    public Music(String album, String artist, String[] songs, double[] prices){
+        mAlbum = album;
+        mArtist = artist;
+        mSongs = songs;
+        mprices = prices;
+
+    }
+}
