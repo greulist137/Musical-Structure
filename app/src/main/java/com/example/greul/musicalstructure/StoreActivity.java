bed2112c25ef92eb35abcb0de149030ab47b8c88
Steven Greulich <greulich.steven@gmail.com>
2018-06-02 07:03:45 -0700

    Commit - Creation of StoreActivity class
    The purpose of this commit is for the creation of the StoreActivity Java class

---
+++ app/src/main/java/com/example/greul/musicalstructure/StoreActivity.java
@@ -0,0 +1,43 @@
+package com.example.greul.musicalstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class StoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+
+        // Create a list of locations
+        final ArrayList<Music> music = new ArrayList<Music>();
+        music.add(new Music("Band", "Album", "Song", 5.99, R.drawable.nature_original));
+
+        // Create an {@link MusicAdapter}, whose data source is a list of {@link Music}s. The
+        // adapter knows how to create list items for each item in the list.
+        MusicAdapter adapter = new MusicAdapter(this, music);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Location} in the list.
+        listView.setAdapter(adapter);
+
+        // Set a click listener to play the audio when the list item is clicked on
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                // Get the {@link Music} object at the given position the user clicked on
+                Music newMusic = music.get(position);
+            }
+        });
+
+    }
+}
